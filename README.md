348834fdb18e2a717b51932943f9520868b5fe4b
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-06-30 15:08:00 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 
 # Fade Effect Gallery
-https://hanabeeeee.github.io/fade-effect-gallery/
 
 ## 기능
 1. 이전/다음 이미지 보기
 2. 인디케이터 버튼 클릭 시 이미지 변경
-3. 현재 이미지 인디케이터 스타일
+3. 현재 이미지 인디케이터 스타일 업데이트
 4. 자동 재생
+
+## 사용 소스
+JS > jQuery, CSS
--- app.js
+++ app.js
@@ -14,10 +14,24 @@ $(function(){
       if(imgCount < 2) {
         imgCount++; // 이미지 번호 증가
         changeSlider(imgCount);
+        updateIndicator(imgCount);
       }
     });
 
-    // 자동 재생
+    // ?초마다 자동 재생
+    setInterval(function(){
+        imgCount++;
+        if (imgCount > 2) {
+            imgCount = 0;
+        }
+        changeSlider(imgCount);
+        updateIndicator(imgCount);
+    }, 4000);
+
+
+    //인디케이터 초기화 (시작)
+    updateIndicator(imgCount)
+
   
     // 이전 이미지 보기
     $('.prevBtn').click(function(){
@@ -34,7 +48,23 @@ let imgCount = 0; // 이미지 번호 -> 전역 함수로 변경
 
 // 하단 인디케이터 버튼을 누르면 해당 이미지(번호) 출력 함수
 function changeSlider(num) {
+    imgCount = num;
     $('.slider img')
-      .fadeOut()
-      .eq(num).fadeIn();
+      .fadeOut(1000)
+      .eq(num).fadeIn(1000);
 };
+
+function updateIndicator(num) {
+    // 인디케이터 스타일 초기화
+    $('.indicator a').css('color', 'grey')
+    .css('background', 'transparent');
+
+    // imgCount 번호 = 인디케이터 번호 (스타일 변경)
+    $('.indicator a')
+    .eq(imgCount)
+    .css('color', 'white')
+    .css('background', 'orange')
+    .css('border-radius', '20px');
+}
+
+
